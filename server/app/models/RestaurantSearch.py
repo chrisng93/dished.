c327bafbcf44b40207bb3e413b159bdafe73b8de
Chris Ng <chrisng@Chriss-MacBook-Pro.local>
2017-04-09 14:22:26 -0700

    delete db_repo, update models, add db_downgrade script

--- server/app/models/RestaurantSearch.py
+++ server/app/models/RestaurantSearch.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from ..app import db
 
 
@@ -7,15 +8,16 @@ TRANSPORT_METHODS = ('DRIVING', 'BICYCLING', 'TRANSIT', 'WALKING')
 
 class RestaurantSearch(db.Model):
     __tablename__ = 'restaurant_search'
-    id = db.Column(db.Integer, primary_key=True)
-    location = db.Column(db.String(400), index=True, nullable=False)
-    transport_method = db.Column(db.Enum(TRANSPORT_METHODS), index=True, nullable=False)
-    desired_travel_time = db.Column(db.Integer, index=True, nullable=False)
-    food_type = db.Column(db.String(120), index=True, nullable=False)
-    selection = db.Column(db.String(120), index=True)
-    rating = db.Column(db.Integer, index=True)
-    comments = db.Column(db.String(1000), index=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    id = db.Column(db.Integer, index=True, primary_key=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow(), nullable=False)
+    user_location = db.Column(db.String(400), nullable=False)
+    transport_method = db.Column(db.Enum(TRANSPORT_METHODS), nullable=False)
+    desired_travel_time = db.Column(db.Integer, nullable=False)
+    food_type = db.Column(db.String(120), nullable=False)
+    selection = db.Column(db.String(120))
+    rating = db.Column(db.Integer)
+    comments = db.Column(db.String(1000))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), index=True)
 
     def __init__(self, location, transport_method, desired_travel_time, food_type):
         self.location = location
--- server/app/models/User.py
+++ server/app/models/User.py
@@ -3,10 +3,11 @@ from ..app import db
 
 class User(db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, index=True, primary_key=True)
     email = db.Column(db.String(120), index=True, unique=True, nullable=False)
-    name = db.Column(db.String(64), index=True)
-    location = db.Column(db.String(400), index=True)
+    name = db.Column(db.String(64))
+    location = db.Column(db.String(400))
+    restaurant_searches = db.relationship('RestaurantSearch', backref='user', lazy='dynamic')
 
     def __init__(self, email):
         self.email = email
