add16425e0eb36e76553f3efaae6d17f98ed3257
Chris Ng <chrisng@Chriss-MacBook-Pro.local>
2017-04-17 19:37:34 -0700

    override update for user to hash passwords

--- server/app/users/__init__.py
+++ server/app/users/__init__.py
@@ -5,5 +5,16 @@ from .UserModel import User
 class UserService(ModelService):
     __model__ = User
 
+    def update(self, **kwargs):
+        """ Updates and returns model """
+        model = self.get(kwargs['id'])
+        for k, v in self._preprocess_params(kwargs).items():
+            if k == 'password':
+                model.set_password(v)
+            else:
+                setattr(model, k, v)
+        self.save(model)
+        return model
+
     def get_by_email(self, email):
         return self.__model__.query.filter_by(email=email).first()
