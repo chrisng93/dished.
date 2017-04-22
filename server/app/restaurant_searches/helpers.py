5e5beb9c83f5f69cb0170e65508cebf6534222ee
Chris Ng <chrisng@Chriss-MacBook-Pro.local>
2017-04-22 14:01:33 -0700

    radius logic

--- server/app/restaurant_searches/helpers.py
+++ server/app/restaurant_searches/helpers.py
@@ -1,77 +1,127 @@
-from urllib.parse import urlparse
 import requests
-import json
-import geopy
-from geopy.distance import VincentyDistance
+from urllib.parse import urlparse
+from math import sqrt, cos, sin, radians, degrees, asin, atan2
 from .. import config
-from ..common.exceptions import UnableToComplete
 
 
-# def get_bound(lat1, lng1, distance, bearing):
-#     origin = geopy.Point(lat1, lng1)
-#     destination = VincentyDistance(miles=distance).destination(origin, bearing)
-#     return destination.latitude, destination.longitude
-#
-#
-# def calc_time(lat1, lng1, distance, bearing, transit_method):
-#     # NOTE: google maps api gives duration back in seconds
-#     lat2, lng2 = get_bound(lat1, lng1, distance, bearing)
-#     res = requests.get('%s/maps/api/distancematrix/json?origins=%s&destinations=%s&mode=%s&key=%s' %
-#                        (config.GOOGLE_API_URL, str(lat1) + ',' + str(lng1), str(lat2) + ',' + str(lng2),
-#                         transit_method, config.GOOGLE_API_KEY))
-#     json = res.json()
-#     info = json['rows'][0]['elements'][0]
-#     if info['status'] == 'ZERO_RESULTS':
-#         return False
-#     return info['duration']['value'] / 60
-#
-#
-# def calculate_radius(lat, lng, travel_time, transit_method, distance=0, bearing=0):
-#     if bearing == 90:
-#         raise UnableToComplete('No surrounding areas found')
-#
-#     if distance == 0:
-#         distance = calc_distance(travel_time, transit_method)
-#     time = calc_time(lat, lng, distance, bearing, transit_method)
-#
-#     if not time:
-#         return get_radius(lat, lng, travel_time, transit_method, distance, bearing + 10)
-#
-#     delta_percentage = (travel_time - time) / time
-#     if abs(delta_percentage) > 0.15:
-#         if time < int(travel_time):
-#             diff = calc_distance(travel_time - time, transit_method)
-#             return get_radius(lat, lng, travel_time, transit_method, distance + diff*0.1, bearing)
-#         else:
-#             diff = calc_distance(time - travel_time, transit_method)
-#             return get_radius(lat, lng, travel_time, transit_method, distance - diff*0.1, bearing)
-#     else:
-#         return distance
-#
-#
-# def calc_distance(desired_time, transit_method):
-#     if transit_method == 'driving':
-#         speed = config.AVERAGE_CAR_SPEED
-#     elif transit_method == 'transit':
-#         speed = config.AVERAGE_TRANSIT_SPEED
-#     elif transit_method == 'bicycling':
-#         speed = config.AVERAGE_BICYCLING_SPEED
-#     else:
-#         speed = config.AVERAGE_WALKING_SPEED
-#     return speed * (desired_time / 60)
+def calculate_distance(geocode1, geocode2):
+    return sqrt((geocode1[0]-geocode2[0])**2 + (geocode1[1]-geocode2[1])**2)
+
+
+def calculate_haversine(origin_geocode, radius, angle):
+    """
+        Use Haversine formula to calculate distance between two points on a sphere. Since we're on earth.
+        And it's round. Sorry Kyrie.
+    """
+    r = 3963.1676  # radius of earth
+    bearing = radians(angle)
+    lat1 = radians(origin_geocode[0])
+    lng1 = radians(origin_geocode[1])
+    lat2 = asin(sin(lat1) * cos(radius / r) + cos(lat1) * sin(radius / r) * cos(bearing))
+    lng2 = lng1 + atan2(sin(bearing) * sin(radius / r) * cos(lat1), cos(radius / r) - sin(lat1) * sin(lat2))
+    lat2 = degrees(lat2)
+    lng2 = degrees(lng2)
+    return [lat2, lng2]
+
+
+def parse_distance_json(url):
+    """ Return addresses/durations given origin, destinations, and transit method """
+    req = requests.get(url)
+    results = req.json()
+    results = [result for result in results if result['status'] == 'OK']
+    addresses = [address for address in results['destination_addresses'] if address != '']
+    i = 0
+    durations = [0] * len(addresses)
+    for row in results['rows'][0]['elements']:
+        if row['status'] == 'OK':
+            if 'duration_in_traffic' in row:
+                durations[i] = row['duration_in_traffic']['value'] / 60
+            else:
+                durations[i] = row['duration']['value'] / 60
+            i += 1
+    return [addresses, durations]
+
 
 def get_geocode(address):
+    """ Get geocode for given address using Google Maps Geocode API """
     prefix = 'https://maps.googleapis.com/maps/api/geocode/json'
-    url = urlparse(prefix + '?address=' + address + '&key=' + config.GOOGLE_API_KEY)
-    full_url = url.scheme + '://' + url.netloc + url.path + '?' + url.query
-    print(full_url)
+    url = urlparse('%s?address=%s&key=%s' % (prefix, address, config.GOOGLE_API_KEY))
+    full_url = '%s://%s%s?%s' % (url.scheme, url.netloc, url.path, url.query)
     req = requests.get(full_url)
-    print(req)
-    d = req.json()
-    print(d)
-    return [d['results'][0]['geometry']['location']['lat'], d['results'][0]['geometry']['location']['lng']]
+    results = req.json()
+    return [results['results'][0]['geometry']['location']['lat'], results['results'][0]['geometry']['location']['lng']]
 
 
-def calculate_radius(address, transit_method, transit_time):
-    geocode = get_geocode(address)
-    print(geocode)
+def get_destination(origin, radius, angle):
+    origin_geocode = get_geocode(origin)
+    return calculate_haversine(origin_geocode, radius, angle)
+
+
+def get_speed(transit_method):
+    transit_method = transit_method.lower()
+    if transit_method == 'driving':
+        return config.AVERAGE_CAR_SPEED
+    elif transit_method == 'transit':
+        return config.AVERAGE_TRANSIT_SPEED
+    elif transit_method == 'bicycling':
+        return config.AVERAGE_BICYCLING_SPEED
+    else:
+        return config.AVERAGE_WALKING_SPEED
+
+
+def build_url(origin, destination, transit_method):
+    """ Build url for isochrone calculation """
+    prefix = 'https://maps.googleapis.com/maps/api/distancematrix/json?'
+    destination_str = str(destination).replace(' ', '')
+    for element in destination:
+        destination_str = '{0}|{1}'.format(destination_str, ','.join(map(str, element)))
+    url = urlparse('%s&origins=%s&destinations=%s&mode=%s&key=%s' % (prefix, origin, destination_str, transit_method, config.GOOGLE_API_KEY))
+    return '%s://%s%s?%s' % (url.scheme, url.netloc, url.path, url.query)
+
+
+def calculate_isochrone(origin, transit_method, transit_time, max_speed=75, num_of_angles=12, tolerance=0.25):
+    speed = get_speed(transit_method)
+    duration = transit_time / speed
+
+    rad1 = [duration] * num_of_angles
+    phi1 = [i * (360/num_of_angles) for i in range(num_of_angles)]
+    data0 = [0] * num_of_angles
+    rad0 = [0] * num_of_angles
+    rmin = [0] * num_of_angles
+    rmax = [(max_speed/60) * transit_time] * num_of_angles
+    iso = [[0, 0]] * num_of_angles
+
+    j = 0
+
+    while sum([a-b for a, b in zip(rad0, rad1)]) != 0:
+        rad2 = [0] * num_of_angles
+        for i in range(num_of_angles):
+            iso[i] = get_destination(origin, rad1[i], phi1[i])
+        full_url = build_url(origin, iso, transit_method)
+        data = parse_distance_json(full_url)
+        for i in range(num_of_angles):
+            if (data[1][i] < (transit_time - tolerance)) & (data0[i] != data[0][i]):
+                rad2[i] = (rmax[i] + rad1[i]) / 2
+                rmin[i] = rad1[i]
+            elif (data[1][i] > (transit_time + tolerance)) & (data0[i] != data[0][i]):
+                rad2[i] = (rmin[i] + rad1[i]) / 2
+                rmax[i] = rad1[i]
+            else:
+                rad2[i] = rad1[i]
+            data0[i] = data[0][i]
+        rad0 = rad1
+        rad1 = rad2
+        j += 1
+        if j > 30:
+            return dict(error='Taking too long'), 500
+    return iso
+
+
+def calculate_radius(origin, transit_method, transit_time):
+    """ Calculate radius around address given transit method and time """
+    origin = origin.replace(' ', '+')
+    origin_geocode = get_geocode(origin)
+    isochrone = calculate_isochrone(origin, transit_method, float(transit_time))
+    distances = [calculate_distance(origin_geocode, destination) for destination in isochrone]
+    average_radius = sum(distances) / len(distances)
+    return dict(radius=average_radius)
