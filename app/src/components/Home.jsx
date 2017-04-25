123f8a7e7e97291f27e6e4c6b7678c3ff3848e74
Chris Ng <chrisng@chriss-mbp.attlocal.net>
2017-04-24 22:20:40 -0700

    progress indicator

--- app/src/components/Home.jsx
+++ app/src/components/Home.jsx
@@ -12,6 +12,12 @@ export default function Home({ routeToLocation }) {
     <section className="home">
       <section className="bg" />
       <section className="home-start-container">
+        <h1 className="home-start-title">
+          dished.
+        </h1>
+        <h3 className="home-start-blurb">
+          Find out what to eat near you
+        </h3>
         <button className="home-start-search" onClick={routeToLocation}>
           Start your search
         </button>
--- app/src/components/ProgressIndicator.jsx
+++ app/src/components/ProgressIndicator.jsx
@@ -8,10 +8,32 @@ const propTypes = {
 };
 
 export default function ProgressIndicator({ currentStep }) {
+  const stepOrder = ['location', 'transit', 'food', 'choices', 'selection'];
+  const currentStepOrder = stepOrder.indexOf(currentStep);
+  console.log(currentStepOrder, stepOrder.indexOf('location'))
   return (
-    <section className="progress-indicator">
-      {currentStep}
-    </section>
+    <ol className="progress-indicator">
+      <li className="progress-indicator-location">
+        <span className="step">Location</span>
+        <span className={`dot ${stepOrder.indexOf('location') <= currentStepOrder ? 'active' : ''}`} />
+      </li>
+      <li className="progress-indicator-transit">
+        <span className="step">Transit</span>
+        <span className={`dot ${stepOrder.indexOf('transit') <= currentStepOrder ? 'active' : ''}`} />
+      </li>
+      <li className="progress-indicator-food">
+        <span className="step">Food</span>
+        <span className={`dot ${stepOrder.indexOf('food') <= currentStepOrder ? 'active' : ''}`} />
+      </li>
+      <li className="progress-indicator-choices">
+        <span className="step">Choices</span>
+        <span className={`dot ${stepOrder.indexOf('choices') <= currentStepOrder ? 'active' : ''}`} />
+      </li>
+      <li className="progress-indicator-selection">
+        <span className="step">Selection</span>
+        <span className={`dot ${stepOrder.indexOf('selection') <= currentStepOrder ? 'active' : ''}`} />
+      </li>
+    </ol>
   );
 }
 
